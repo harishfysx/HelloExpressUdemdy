eeeb0f90047a93748f456e4d5f18634ee3b9883a
root <root@zenbership126.harishfysx.bpm>
2017-05-25 10:28:17 -0400

    FIrst Commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const hbs = require('hbs');
+const app = express();
+const fs = require('fs');
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+app.use(express.static( __dirname + '/public'));
+
+app.use((req,res,next) =>{
+  var now = new Date().toString();
+  var log = `${now} : ${req.method} ${req.ip} ${req.url}`
+  console.log(log);
+  fs.appendFile('server.log',log + '\n',(err) =>{
+    if(err){
+      console.log('Unable to append to server.log');
+    }
+  })
+
+  next();
+});
+
+app.use((req,res,next) =>{
+  res.render('ucs');
+  //next();
+});
+
+
+hbs.registerHelper('currentYear' , () =>{
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('upcaseIt' , (text) =>{
+    return text.toUpperCase();
+});
+
+
+app.get('/', function (req, res) {
+  res.render('index',{
+    pageTitle : 'Welcome',
+    welcomeMessage : 'Welcome to my website'
+  });
+});
+
+app.get('/about', function (req, res) {
+  res.render('about',{
+    pageTitle : 'About',
+    welcomeMessage : 'Welcome to my website'
+  });
+});
+
+
+
+app.get('/bad',(req,res) =>{
+
+  res.send({errorMessage :'bad request'});
+} );
+
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+});
+
+//test
